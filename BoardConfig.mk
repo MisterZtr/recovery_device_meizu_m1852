cc5531ec79c5304971ac333d920d18661205a9a6
MisterZtr <andey2004586@gmail.com>
2022-07-01 06:04:35 +0300

    m1852: encryptable footer

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,12 @@ TARGET_USES_UEFI := true
 
 BOARD_AVB_ENABLE := true
 
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
+TW_INCLUDE_CRYPTO := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 swiotlb=1
